97f38c2872dcc0c61dadf5399fa9717c6bed37bf
Fayaz Ali <108985977+CsNinja34@users.noreply.github.com>
2024-03-01 01:27:39 +0100

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Hash;
+use Spatie\Permission\Traits\HasRoles;
+use DB;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user= User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('codeastro.com'),
+        ]);
+        $role = Role::create([
+            'slug' => 'admin',
+            'name' => 'Adminstrator',
+        ]);
+        $user->roles()->sync($role->id);
+    }
+}
